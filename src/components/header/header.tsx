afaa9dbb66171ea4a2e169c12204d8557578c504
Ivan Isayenko <ivanisayenko@gmail.com>
2026-06-19 15:07:15 +0400

    refactor: naming of classes of header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import "./header.css";
 import logo from "@/assets/icons/logo.svg";
 import bag from "@/assets/icons/bag.svg";
+import { useShopping } from "@/context/shopping-context";
 
 export default function Header() {
+  const { shoppingList, isLoading } = useShopping();
+  const shoppingCount = shoppingList.reduce((total, item) => total + item.quantity, 0);
+
   return (
-    <header className="header">
-      <Image src={logo} alt="Logo" />
-      <div className="cart-container">
-        <Image src={bag} alt="Bag" />
-        <span className="cart-count">0</span>
+    <header className={`header ${isLoading ? "header--loading" : ""}`}>
+      <Image src={logo} alt="Logo" width={74} height={24} />
+      <div className="header__cart">
+        <Image src={bag} alt="Shopping Bag" width={18} height={18} />
+        <span className="header__cart-count">{shoppingCount}</span>
       </div>
     </header>
   );
