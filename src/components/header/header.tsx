8d5c249978c7a6bcdb23cc42b8fc5f43b78c26a1
Ivan Isayenko <ivanisayenko@gmail.com>
2026-06-19 18:13:46 +0400

    feat: implementation of loading state to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import Image from "next/image";
+import type { CSSProperties } from "react";
 import "./header.css";
 import logo from "@/assets/icons/logo.svg";
 import bag from "@/assets/icons/bag.svg";
-import { useShopping } from "@/context/shopping-context";
+import { useLoading } from "@/context/loading-context";
 
 export default function Header() {
-  const { shoppingList, isLoading } = useShopping();
-  const shoppingCount = shoppingList.reduce((total, item) => total + item.quantity, 0);
+  const { loadingProgress } = useLoading();
+  const loadingStyle = {
+    "--header-loading-progress": loadingProgress / 100,
+  } as CSSProperties;
 
   return (
-    <header className={`header ${isLoading ? "header--loading" : ""}`}>
+    <header
+      className={`header ${loadingProgress > 0 ? "header--loading" : ""}`}
+      style={loadingStyle}
+    >
       <Image src={logo} alt="Logo" width={74} height={24} />
       <div className="header__cart">
         <Image src={bag} alt="Shopping Bag" width={18} height={18} />
-        <span className="header__cart-count">{shoppingCount}</span>
+        <span className="header__cart-count">{0}</span>
       </div>
     </header>
   );
