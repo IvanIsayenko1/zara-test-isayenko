88345746ff1e7e6a7560137d767d8c75c1464542
Ivan Isayenko <ivanisayenko@gmail.com>
2026-06-19 18:14:26 +0400

    feat: showing fetched products on smartphone list

--- src/components/smartphone-list/smartphone-list.tsx
+++ src/components/smartphone-list/smartphone-list.tsx
@@ -1,14 +1,17 @@
+"use client";
+
+import { useProducts } from "@/context/products-context";
 import "./smartphone-list.css";
 import { SmarthponeCard } from "../smartphone-card/smartphone-card";
 
 export function SmartphoneList() {
+  const { products } = useProducts();
+
   return (
     <div className="smartphone-list">
-      <SmarthponeCard />
-      <SmarthponeCard />
-      <SmarthponeCard />
-      <SmarthponeCard />
-      <SmarthponeCard />
+      {products.map((product, index) => (
+        <SmarthponeCard key={`${product.id}-${index}`} product={product} />
+      ))}
     </div>
   );
 }
