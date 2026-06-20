518aac207a1d05c014b9e4cf37df62dd67194284
Ivan Isayenko <ivanisayenko@gmail.com>
2026-06-20 20:44:16 +0400

    feat: implement the navigation by clicking the logo of the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,9 +6,11 @@ import "./header.css";
 import logo from "@/assets/icons/logo.svg";
 import bag from "@/assets/icons/bag.svg";
 import { useLoading } from "@/context/loading-context";
+import Link from "next/link";
 
 export default function Header() {
   const { loadingProgress } = useLoading();
+
   const loadingStyle = {
     "--header-loading-progress": loadingProgress / 100,
   } as CSSProperties;
@@ -18,7 +20,9 @@ export default function Header() {
       className={`header ${loadingProgress > 0 ? "header--loading" : ""}`}
       style={loadingStyle}
     >
-      <Image src={logo} alt="Logo" width={74} height={24} />
+      <Link href="/">
+        <Image src={logo} alt="Logo" width={74} height={24} />
+      </Link>
       <div className="header__cart">
         <Image src={bag} alt="Shopping Bag" width={18} height={18} />
         <span className="header__cart-count">{0}</span>
