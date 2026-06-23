539c18ffad3c64b81e2e1e829a294a233a21f8bc
Ivan Isayenko <ivanisayenko@gmail.com>
2026-06-23 15:11:20 +0400

    feature: adding a feature flags to test the different scenarios during the fetching

--- src/services/products.ts
+++ src/services/products.ts
@@ -4,6 +4,12 @@ const PRODUCTS_API_URL = "https://prueba-tecnica-api-tienda-moviles.onrender.com
 
 const PRODUCTS_API_KEY = "87909682e6cd74208f41a6ef39fe4191";
 
+const SHOULD_SIMULATE_PRODUCT_ERROR = false;
+const SHOULD_SIMULATE_PRODUCT_DELAY = false;
+const PRODUCT_FETCH_DELAY = 3000;
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export async function fetchProducts({
   limit = 20,
   search = "",
@@ -11,6 +17,14 @@ export async function fetchProducts({
   limit?: number;
   search?: string;
 }): Promise<Product[]> {
+  if (SHOULD_SIMULATE_PRODUCT_DELAY) {
+    await delay(PRODUCT_FETCH_DELAY);
+  }
+
+  if (SHOULD_SIMULATE_PRODUCT_ERROR) {
+    throw new Error("Simulated products fetch error");
+  }
+
   const response = await fetch(`${PRODUCTS_API_URL}?limit=${limit}&search=${search}`, {
     next: { revalidate: 300 },
     headers: {
@@ -26,6 +40,14 @@ export async function fetchProducts({
 }
 
 export async function fetchProduct(id: string): Promise<ProductDetail> {
+  if (SHOULD_SIMULATE_PRODUCT_DELAY) {
+    await delay(PRODUCT_FETCH_DELAY);
+  }
+
+  if (SHOULD_SIMULATE_PRODUCT_ERROR) {
+    throw new Error("Simulated product fetch error");
+  }
+
   const response = await fetch(`${PRODUCTS_API_URL}/${id}`, {
     cache: "no-store",
     headers: {
